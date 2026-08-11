b842af78b1b6194fd216ff2a2166a66ffb96b395
Betakontext <dev@betakontext.de>
2026-08-11 13:01:11 +0200

    readme update

--- snapsplit/__init__.py
+++ snapsplit/__init__.py
@@ -23,7 +23,7 @@ bl_info = {
     "name": "SnapSplit – Print-ready segmentation with connectors",
     "author": "https://dev.betakontext.de | Christoph Medicus | dev@betakontext.de",
     "version": (0, 1, 3),
-    "blender": (5, 0, 1),
+    "blender": (5, 2, 0),
     "location": "View3D > N-Panel > SnapSplit",
     "description": (
         "Split meshes into printable parts and generate fitting connectors for 3D printing."
